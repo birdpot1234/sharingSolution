9c7d8ff37f6338289168171a811d68a0062dc17d
BirdPeerapon <peerapon.pot@dplusonline.net>
2019-05-12 19:33:31 +0700

    creat api

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/api/test', function(req, res, next) {
+  // res.render('index', { title: 'Express' });
+  res.status(200).json({success:true,message:"success"})
+});
+
+
+module.exports = router;
