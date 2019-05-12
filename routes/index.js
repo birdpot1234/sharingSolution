b50728226cf00d61d8d2d114176a77c4ca006f62
BirdPeerapon <peerapon.pot@dplusonline.net>
2019-05-12 20:35:31 +0700

    edit api

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/api/test', function(req, res, next) {
   // res.render('index', { title: 'Express' });
-  res.status(200).json({success:true,message:"success"})
+  res.status(200).json({success:true,message:"Success Production"})
 });
 
 
